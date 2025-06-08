34ae30a53d815959f83b35ba4cc2eed04ec979e9
phoebeobaje <phoeberobertscareer@gmail.com>
2025-06-09 00:41:39 +0100

    interface

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,12 +1,5 @@
-## components/common/Button.tsx
 
-interface ButtonProps {
-  buttonLabel: string
-  buttonSize?: string
-  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
-  action?: () => void
-}
-
+import { ButtonProps } from "@/interface";
 
 const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
 
